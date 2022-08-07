2a2e35021ceae6eae8811b37679ecf535e2539e2
j6kim2026 <j6kim2026@chadwickschool.org>
2022-08-07 17:39:51 +0900

    Upload isPrime()

--- tutorial.py
+++ tutorial.py
@@ -1,1 +1,11 @@
-print("Hello")
+def isPrime(x):
+	if x == 0 or x == 1:
+		return False
+	elif x == 2:
+		return True
+	else:
+		for i in range(2,x):
+			if x % i == 0:
+				return False
+				break
+	return Trues
