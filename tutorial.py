b43a0a00f1340c1b603d558fdee7c39bf14036d8
Suri <jin@Suris-MacBookPro.local>
2022-08-07 17:40:50 +0900

    isPrime

--- tutorial.py
+++ tutorial.py
@@ -1,1 +1,16 @@
 print("Hello")
+
+def isPrime(n):
+    if n == 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, n//2, 2):
+        if (n%i) == 0: 
+            return False
+    return True
+
+
+
