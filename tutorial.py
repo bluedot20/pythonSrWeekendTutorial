8a1c64ffc9a71d108d9b3ab39d58659e621df5a8
Jimin Lee <student@Jimin-Lees-Student-MacBook-2.local>
2022-08-07 18:07:02 +0900

    isPrime

--- tutorial.py
+++ tutorial.py
@@ -1,1 +1,15 @@
-print("Hello")
+def isPrime(num):
+    if num>1:
+    	if num ==2:
+    		return True
+    	else:
+        	for i in range(2, num):
+            	if num%i==0:
+                	return False
+        	return True
+
+
+
+assert(isPrime(11)==True)
+assert(isPrime(13)==True)
+assert(isPrime(12)==False)
